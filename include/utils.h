c1f1a2d3536b3be399824e815a77a27071c4f4d4
Hamad Numan <78150971+hamad-9@users.noreply.github.com>
2024-02-19 11:41:56 +0200

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -3,20 +3,16 @@
 
 #include <string>
 #include <vector>
-#include "DateTime.h" // Include the DateTime class definition
+#include "DateTime.h"
 
 bool isLeapYear(int year);
 
 int daysSinceEpoch(int year, int month, int day);
 
-bool validDateTimeFormat(std::string &value);
-
-bool undefined(std::string &value);
-
-long long parseDateTimeToEpoch(const std::string &dateTime);
-
 void quickSort(std::vector<DateTime>& arr, int low, int high);
 
 int partition(std::vector<DateTime>& arr, int low, int high);
+
+long long dateTimeToEpoch(const DateTime& dateTime);
 
 #endif // UTILS_H
